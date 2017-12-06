a60d8fc202ed2aaf67836a45750ef87c4f7565d8
josesanchez19 <33655354+josesanchez19@users.noreply.github.com>
2017-12-06 11:26:25 +0100

    Add files via upload

---
+++ python/practicaexamen.py
@@ -0,0 +1,12 @@
+def saludador3():
+    c="chica"
+    v="chico"
+    nombre=raw_input("Dime tu nombre")
+    print ("bonito nombre"),nombre
+    x=input("cuantos hermanos tienes")
+    if x==1:
+        y=raw_input("chico o chica")
+        if y==c:
+            b=raw_input("como se llama tu hermana")
+            print b,("seguro que es adoptada")
+saludador3()
